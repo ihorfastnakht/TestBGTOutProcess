f4ef2ca90794bd9a5f4fd30c8256da6a4f594fec
ihorfastnakht <fastnakht.corp@gmail.com>
2017-04-24 16:07:24 +0300

    Add project files.

---
+++ BackgroundComponent/TimeBackgroundTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+
+namespace BackgroundComponent
+{
+    public sealed class TimeBackgroundTask : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+            await SendToast();
+            deferral.Complete();
+        }
+
+        private Task SendToast()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var notificationXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+                var toeastElement = notificationXml.GetElementsByTagName("text");
+                toeastElement[0].AppendChild(notificationXml.CreateTextNode("TestBGT-OutProcess"));
+                toeastElement[1].AppendChild(notificationXml.CreateTextNode($"{DateTime.Now.ToString("HH:mm:ss")}"));
+                var toastNotification = new ToastNotification(notificationXml);
+                ToastNotificationManager.CreateToastNotifier().Show(toastNotification);
+            });
+        }
+    }
+}
---
+++ TestBGTOutProcess/BackgroundTaskWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Microsoft.Toolkit.Uwp;
+
+namespace TestBGTOutProcess
+{
+    public static class BackgroundTaskWrapper
+    {
+        public static void RegisterTasks()
+        {
+            var taskName = nameof(BackgroundComponent.TimeBackgroundTask);
+            var taskEntryPoint = typeof(BackgroundComponent.TimeBackgroundTask).ToString();
+            var trigger = new TimeTrigger(15, false);
+            var condition = new SystemCondition(SystemConditionType.InternetAvailable);
+
+            BackgroundTaskHelper.Register(taskName, taskEntryPoint, trigger, true, conditions: condition);
+        }
+    }
+}
